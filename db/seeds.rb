b7a9104a284a8cf6b357aea26b2968f4748d0af4
JoeJorgensen <joebjorg@gmail.com>
2022-03-10 14:53:54 -0700

    movies added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Movie.create(title:'Interstellar', stars: 4.5)
+Movie.create(title:'1917', stars: 4)
+Movie.create(title:'Inception', stars: 4.9)
+Movie.create(title:'Harry Potter', stars: 3.9)
+Movie.create(title:'Lighthouse', stars: 3.7)
+
+
+
+
