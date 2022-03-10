7c3bf89511191587d47ccd7a8c60d79e3b55ee97
Sharayah Hefner <rocknrayah@gmail.com>
2022-03-10 16:27:47 -0700

    added libraries

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {link,Outlet} from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <link to='/'>Home</link> {'-'}
+       <link to='/movies'>Movies</link>{'-'}
+        
     </div>
   );
 }
