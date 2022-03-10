10ca386f58bf4efdd2e05933edb8798267692aad
Katherine <katbrenda1988@gmail.com>
2022-03-10 16:56:27 -0700

    fixed app.js error

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import { Link, Outlet } from 'react-router-dom';
-=======
-import {link,Outlet} from 'react-router-dom'; 
->>>>>>> 7c3bf89511191587d47ccd7a8c60d79e3b55ee97
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <nav>
         <Link to='/'>Home</Link>{'- '}
         <Link to='/about'>About</Link>{'- '}
@@ -18,11 +13,6 @@ function App() {
         <Link to='/moviesShow'>MoviesShow</Link>
       </nav>
       <Outlet />
-=======
-     <link to='/'>Home</link> {'-'}
-       <link to='/movies'>Movies</link>{'-'}
-        
->>>>>>> 7c3bf89511191587d47ccd7a8c60d79e3b55ee97
     </div>
   );
 }
