07dd0adac738d9dae0ed1198d80c25447325e5c2
Sharayah Hefner <rocknrayah@gmail.com>
2022-03-10 17:03:33 -0700

    saved

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,14 @@ import { Link, Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-  <nav> 
-     <Link to='/'>Home</Link> {'-'}
-        <Link to='/movies'>Movies</Link>{'-'}
-        <Link to='/character'>Characters</Link>{'-'}
+      <nav>
+        <Link to='/'>Home</Link>{'- '}
+        <Link to='/about'>About</Link>{'- '}
+        <Link to='/movie'>Movie</Link>{'- '}
+        <Link to='/movies'>Movies</Link>{'- '}
+        <Link to='/moviesShow'>MoviesShow</Link>
       </nav>
-      <Outlet/>
-
-     
-      
-        
-
+      <Outlet />
     </div>
   );
 }
